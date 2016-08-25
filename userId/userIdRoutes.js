48a4e9e426124d8d7397db705e5f29806ffeb439
Brett Celestre <bcelestre@gmail.com>
2016-08-24 18:13:33 -0700

    Completes userId API call

--- userId/userIdRoutes.js
+++ userId/userIdRoutes.js
@@ -1,12 +1,20 @@
 
-var server = require('../server.js');
+var server = require('../server.js'),
+    data = require('../data.js');
 
 module.exports = function(app) {
   
   app.route('/:id')
     .get(function (req, res, next) {
-      var id = req.params.id;
-      res.send('User ID = ' + id);
+      var id = req.params.id,
+          usersItems = {'userIdData': []};
+      
+      data.data.forEach(function(val) {
+        if ( val.userId === id ) {
+          usersItems.userIdData.push(val);
+        }
+      })
+      res.send(usersItems);
     });
     
 };
